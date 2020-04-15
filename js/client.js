a0dd7e3fe3cc0952d2c63d36ac2953dd2fac3145
pjayathissa <p.jayathissa@gmail.com>
2020-04-15 19:07:20 +1200

    first commit of survey

---
+++ js/client.js
@@ -0,0 +1,49 @@
+function checkForm() {
+  if (document.getElementById("A").checked) {
+    document.getElementById("out").innerHTML = "Value is A";
+  } else {
+    document.getElementById("out").innerHTML = "Value is B";
+  }
+}
+
+var rangeSlider = function() {
+  //var slider = document.getElementById("myRange");
+  const textSelection = [
+    "All of the time",
+    "Most of the time",
+    "More than half the time",
+    "Less than half the time",
+    "Some of the time",
+    "At no time",
+  ];
+  //var output = document.getElementById("rangevalue");
+
+  var sliders = Array.from(document.getElementsByClassName("rangeClass"));
+
+  console.log(sliders);
+  //console.log(output);
+
+  sliders.forEach((slider) => {
+    console.log(slider.value)
+    var rangeName = slider.id + "-value"
+    document.getElementById(slider.id + "-value").value = textSelection[slider.value];
+
+
+    
+  });
+
+  let finalScore = sliders.reduce((total,b) => total + parseInt(b.value), 0)
+  document.getElementById("final-score").value =  finalScore
+
+  if (finalScore > 13) {
+    document.getElementById("score-above-13").style.display = "inline"
+    document.getElementById("score-below-13").style.display = "none"
+
+  }
+  else {
+    document.getElementById("score-below-13").style.display = "inline"
+    document.getElementById("score-above-13").style.display = "none"
+  }
+};
+
+rangeSlider();
