79bc9c5a872ed7177fd269feb0b269dbad9219db
pjayathissa <p.jayathissa@gmail.com>
2020-04-22 17:51:34 +1200

    added new images and logo and styling update

--- js/client.js
+++ js/client.js
@@ -1,13 +1,12 @@
 // convert json object to json array
-var linksArray = Object.values(links)
-
-console.log(linksArray)
+var linksArray = Object.values(links);
 
 var rangeSlider = function() {
-  linksArray.map(link => {
-    link.total = 0
-  })
-  //var slider = document.getElementById("myRange");
+  linksArray.map((link) => {
+    link.total = 0;
+  });
+
+  // text that is displayed above the slider
   const textSelection = [
     "All of the time",
     "Most of the time",
@@ -16,76 +15,69 @@ var rangeSlider = function() {
     "Some of the time",
     "At no time",
   ];
-  //var output = document.getElementById("rangevalue");
 
+  // get all sliders
   var sliders = Array.from(document.getElementsByClassName("rangeClass"));
 
+  //itterate through sliders and adjust value above it to meet the text
   sliders.forEach((slider) => {
-    var rangeName = slider.id + "-value"
-    document.getElementById(slider.id + "-value").value = textSelection[slider.value];
+    var rangeName = slider.id + "-value";
+    document.getElementById(slider.id + "-value").value =
+      textSelection[slider.value];
+    // if "never" is selected it is an emergency
     if (slider.value > 4) {
-      document.getElementById(slider.id + "-info").style.display = "block"
+      document.getElementById(slider.id + "-info").style.display = "block";
     } else {
-      document.getElementById(slider.id + "-info").style.display = "none"
+      document.getElementById(slider.id + "-info").style.display = "none";
     }
 
     // Calculate the total score of each link base on the slider value
     linksArray.map((link) => {
-      link.total += link[slider.id] * slider.value
-    })
-    //console.log(linksArray)
-
-    
+      link.total += link[slider.id] * slider.value;
+    });
   });
 
-  let finalScore = sliders.reduce((total,b) => total + parseInt(b.value), 0)
-  document.getElementById("final-score").value =  finalScore
+  let finalScore = sliders.reduce((total, b) => total + parseInt(b.value), 0);
+  document.getElementById("final-score").value = finalScore;
 
   // Sort arrays based on their score
-  linksArray.sort((a,b) =>{
-    return b.total - a.total
-  })
+  linksArray.sort((a, b) => {
+    return b.total - a.total;
+  });
 
-  console.log(linksArray)
-  
-
-
+  // if people are not doing well
   if (finalScore > 13) {
+    // the two lists that need to be dynamically modified
     var topTwoList = document.getElementById("top-two-list");
-    var restList = document.getElementById("rest-list")
-    topTwoList.innerHTML = ''
-    restList.innerHTML = ''
+    var restList = document.getElementById("rest-list");
+    // reset them no empty
+    topTwoList.innerHTML = "";
+    restList.innerHTML = "";
 
-    counter = 0
-    linksArray.forEach(link => {
-      var div = document.createElement("div")
+    counter = 0;
+    linksArray.forEach((link) => {
+      var div = document.createElement("div");
       var li = document.createElement("a");
-      li.setAttribute('href',link.url);
-      li.setAttribute('target',"_blank");
-      li.setAttribute('class',"link custom-blue underline dim");
-      li.setAttribute('id',link.name);
+      li.setAttribute("href", link.url);
+      li.setAttribute("target", "_blank");
+      li.setAttribute("class", "link custom-blue underline dim");
+      li.setAttribute("id", link.name);
       li.appendChild(document.createTextNode(link.name));
-      div.appendChild(li)
+      div.appendChild(li);
       if (counter < 2) {
         topTwoList.appendChild(div);
-      }
-      else {
+      } else {
         restList.appendChild(div);
       }
-      
-      counter++
 
-    })
+      counter++;
+    });
 
-
-
-    document.getElementById("score-above-13").style.display = "block"
-    document.getElementById("score-below-13").style.display = "none"
-
-  }
-  else {
-    document.getElementById("score-below-13").style.display = "block"
-    document.getElementById("score-above-13").style.display = "none"
+    document.getElementById("score-above-13").style.display = "block";
+    document.getElementById("score-below-13").style.display = "none";
+  } else {
+    // people are doing well
+    document.getElementById("score-below-13").style.display = "block";
+    document.getElementById("score-above-13").style.display = "none";
   }
 };
-
