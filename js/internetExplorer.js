9d47a8f8ca7bfda8740c0114cf62376541cf95c8
pjayathissa <p.jayathissa@gmail.com>
2020-04-28 15:08:28 +1200

    ie console log

--- js/internetExplorer.js
+++ js/internetExplorer.js
@@ -4,6 +4,7 @@ function isIE() {
 	var is_ie = ua.indexOf("MSIE ") > -1 || ua.indexOf("Trident/") > -1;
 
 	if (is_ie) {
+		console.log("It is InternetExplorer");
 		document.getElementById("ieCheck").style.display = "block";
 	} else {
 		console.log("It is NOT InternetExplorer");
